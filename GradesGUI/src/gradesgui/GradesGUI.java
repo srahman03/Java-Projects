87f0502c4d332c78c42b8d8e1e666374c0814939
srahman03 <116261624+srahman03@users.noreply.github.com>
2023-06-16 19:22:41 +0100

    Add files via upload

---
+++ GradesGUI/src/gradesgui/GradesGUI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gradesgui;
+
+/**
+ *
+ * @author sadik
+ */
+public class GradesGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     NewJFrame run = new NewJFrame();
+
+    }
+    
+}
